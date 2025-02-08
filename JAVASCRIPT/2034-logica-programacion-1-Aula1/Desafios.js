7f65b85c0870a884189cfcb26eab5092ad94665f
Mau-Mech <mauriciojimenez20@hotmail.com>
2025-02-07 18:06:59 -0800

    Add files via upload

---
+++ JAVASCRIPT/2034-logica-programacion-1-Aula1/Desafios.js
@@ -0,0 +1,74 @@
+prompt("¡Bienvenida y bienvenido a nuestro sitio web!");
+
+let nombre = "Lua";
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+
+alert("¡Error! Completa todos los campos");
+
+
+let mensajeDeError = "¡Error! Completa todos los campos";
+alert(mensajeDeError);
+
+
+let nombre2 =  prompt("Whats your name?: ");
+
+let myAge =  prompt("Whats your age?: ");
+
+if (myAge >= 18) {
+
+    alert("¡Puedes obtener tu licencia de conducir!");
+}
+
+
+
+// desafios seccion 2
+
+
+// Desafio 1:
+
+let dia = prompt("Que dia de la semana es?: ")
+
+if (dia = "Sabado") {
+    alert("¡Buen fin de semana!");
+} else if (dia = "Domingo") {
+    alert("¡Buen fin de semana!");
+} else{
+    alert( "¡Buena semana!");
+}
+
+// Desafio 2:
+let number = prompt("Type any number: ")
+
+if (number >= 0) {
+    alert("The number its positive")
+} else {
+    alert("The number its negative")
+}
+
+// Desafio 4:
+
+let saldo = 1000
+
+alert(`Your current money its: ${saldo}`);
+
+// Desafio 5:
+
+let nameOwner = promp("Please ty your name: ");
+
+alert(`Welcome ${nameOwner}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JAVASCRIPT/2034-logica-programacion-1-Aula1/Desafios_2.js
@@ -0,0 +1,32 @@
+// Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+
+let number = 1
+while (number < 10){
+    console.log(number);
+    number++;
+} 
+
+//Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
+
+let number2 = 10;
+while (number2 >= 0){
+    console.log(number);
+    number--;
+} 
+
+//Crea un programa de cuenta regresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+
+number3 = prompt("Please give a number: ");
+while (number3 >= 0){
+    console.log(number);
+    number--;
+} 
+
+//Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+
+number4 = prompt("Please give a number: ")
+control = 0
+while (control < number4 ){
+    console.log(control);
+    control ++;
+} 
